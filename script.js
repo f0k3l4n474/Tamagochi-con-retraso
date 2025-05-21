eac3a1d3ec10c77ec2dc6cf523f5da9bf2020b5b
f0k3l4n474 <ainfrod0411@gmail.com>
2025-05-22 01:58:37 +0200

    icons-bars

---
+++ script.js
@@ -0,0 +1,7 @@
+const hungerBtn = document.getElementById('hunger-button');
+const gamesBtn = document.getElementById('games-button');
+const shoppingBtn = document.getElementById('shopping-button');
+
+const hungerBar = document.getElementById('hunger-bar');
+const gamesBar = document.getElementById('games-bar');
+const shoppingBar = document.getElementById('shopping-bar');
